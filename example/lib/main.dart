f8b6e1ab9da13596356adc855756c906bb8b8d64
Konstantin Kinzel <konstantin.kinzel@phellowseven.com>
2023-09-22 19:11:06 +0200

    DEV: temporary setting to reproduce @available-issue
    Swift Compiler Error (Xcode): Stored properties cannot be marked unavailable with '@available'
    /document_scanner_flutter/example/ios/Pods/WeScan/WeScan/ImageScannerController.swift:196:5

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:document_scanner_flutter/document_scanner_flutter.dart';
 import 'package:document_scanner_flutter/configs/configs.dart';
 import 'package:flutter/material.dart';
-import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
+// import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  PDFDocument? _scannedDocument;
+  // PDFDocument? _scannedDocument;
   File? _scannedDocumentFile;
   File? _scannedImage;
 
@@ -31,11 +31,11 @@ class _MyAppState extends State<MyApp> {
       //source: ScannerFileSource.CAMERA
     );
     if (doc != null) {
-      _scannedDocument = null;
+      // _scannedDocument = null;
       setState(() {});
       await Future.delayed(Duration(milliseconds: 100));
       _scannedDocumentFile = doc;
-      _scannedDocument = await PDFDocument.fromFile(doc);
+      // _scannedDocument = await PDFDocument.fromFile(doc);
       setState(() {});
     }
   }
@@ -64,21 +64,21 @@ class _MyAppState extends State<MyApp> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            if (_scannedDocument != null || _scannedImage != null) ...[
-              if (_scannedImage != null)
-                Image.file(_scannedImage!,
-                    width: 300, height: 300, fit: BoxFit.contain),
-              if (_scannedDocument != null)
-                Expanded(
-                    child: PDFViewer(
-                  document: _scannedDocument!,
-                )),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                    _scannedDocumentFile?.path ?? _scannedImage?.path ?? ''),
-              ),
-            ],
+            // if (_scannedDocument != null || _scannedImage != null) ...[
+            //   if (_scannedImage != null)
+            //     Image.file(_scannedImage!,
+            //         width: 300, height: 300, fit: BoxFit.contain),
+            //   if (_scannedDocument != null)
+            //     Expanded(
+            //         child: PDFViewer(
+            //       document: _scannedDocument!,
+            //     )),
+            //   Padding(
+            //     padding: const EdgeInsets.all(8.0),
+            //     child: Text(
+            //         _scannedDocumentFile?.path ?? _scannedImage?.path ?? ''),
+            //   ),
+            // ],
             Center(
               child: Builder(builder: (context) {
                 return ElevatedButton(
